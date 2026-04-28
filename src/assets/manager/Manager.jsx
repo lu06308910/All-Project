f263c303b2218f32f137e157853eaad402f0b581
rohga19 <rohga19@gmail.com>
2026-04-28 17:35:14 +0900

    상품정보수정중

--- src/assets/manager/Manager.jsx
+++ src/assets/manager/Manager.jsx
@@ -20,7 +20,7 @@ function Manager(){
                                                 style={{ 
                                                         cursor: 'pointer', 
                                                         backgroundColor: activeMenu === menu ? '#3a4ca8' : '#333333',
-                                                        padding: activeMenu == menu ? '10px' : '0px'
+                                                        padding: '10px 15px'
                                                 }}
                                         >
                                         {menu}
@@ -28,60 +28,188 @@ function Manager(){
                                 ))}
                                 </div>
                         </div>
-                        <div style={{display: 'flex', flexDirection: 'column', width:'1000px'}}>
-                                <div className='category-content'>
-                                        {activeMenu == '회원 관리' && (
-                                                <>
-                                                        <h4 style={{textAlign:'left'}}>회원 검색</h4>
-                                                        <div class="row" style={{backgroundColor:'#eeeeee'}}>
-                                                                <div className="col p-2">번호</div>
-                                                                <div className="col p-2">아이디</div>
-                                                                <div className="col p-2">성명</div>
-                                                                <div className="col p-2">이메일</div>
-                                                                <div className="col p-2">연락처</div>
-                                                                <div className="col p-2">가입일</div>
-                                                                <div className="col p-2">관리</div>
+
+                        {/* 회원관리 페이지 */}
+                        {activeMenu == '회원 관리' &&(
+                                <div style={{display: 'flex', flexDirection: 'column', width:'900px'}}>
+                                        <div className='category-content'>
+                                                <h4 style={{textAlign:'left', fontWeight:'600'}}>회원 검색</h4>
+                                                <div class="row" style={{backgroundColor:'#eeeeee', fontSize:'0.8em'}}>
+                                                        <div className="col p-2">번호</div>
+                                                        <div className="col p-2">아이디</div>
+                                                        <div className="col p-2">성명</div>
+                                                        <div className="col p-2">이메일</div>
+                                                        <div className="col p-2">연락처</div>
+                                                        <div className="col p-2">가입일</div>
+                                                        <div className="col p-2">관리</div>
+                                                </div>
+                                                <div class="row" style={{fontSize:'0.8em'}}>
+                                                        <div className="col p-2">*번호</div>
+                                                        <div className="col p-2">*아이디</div>
+                                                        <div className="col p-2">*성명</div>
+                                                        <div className="col p-2">*이메일</div>
+                                                        <div className="col p-2">*연락처</div>
+                                                        <div className="col p-2">*가입일</div>
+                                                        <div className="col p-2">*관리</div>
+                                                </div>
+                                                <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                                                        <div style={{textDecoration:'underline', textAlign:'left', fontSize:'0.8em'}}>
+                                                                더보기
                                                         </div>
-                                                        <div class="row">
-                                                                <div className="col p-2">*번호</div>
-                                                                <div className="col p-2">*아이디</div>
-                                                                <div className="col p-2">*성명</div>
-                                                                <div className="col p-2">*이메일</div>
-                                                                <div className="col p-2">*연락처</div>
-                                                                <div className="col p-2">*가입일</div>
-                                                                <div className="col p-2">*관리</div>
-                                                        </div>
-                                                </>
-                                                )
-                                        }
+                                                        <div className='button'>탈퇴처리</div>
+                                                </div>
+                                        </div>
+                                        <div className='category-content'>
+                                                <h4 style={{textAlign:'left', fontWeight:'600'}}>탈퇴회원 검색</h4>
+                                                <div class="row" style={{backgroundColor:'#eeeeee', fontSize:'0.8em'}}>
+                                                        <div className="col p-2">번호</div>
+                                                        <div className="col p-2">아이디</div>
+                                                        <div className="col p-2">성명</div>
+                                                        <div className="col p-2">이메일</div>
+                                                        <div className="col p-2">연락처</div>
+                                                        <div className="col p-2">가입일</div>
+                                                        <div className="col p-2">관리</div>
+                                                </div>
+                                                <div class="row" style={{fontSize:'0.8em'}}>
+                                                        <div className="col p-2">*번호</div>
+                                                        <div className="col p-2">*아이디</div>
+                                                        <div className="col p-2">*성명</div>
+                                                        <div className="col p-2">*이메일</div>
+                                                        <div className="col p-2">*연락처</div>
+                                                        <div className="col p-2">*가입일</div>
+                                                        <div className="col p-2">*관리</div>
+                                                </div>
+                                                <p style={{textDecoration:'underline', textAlign:'left', fontSize:'0.8em'}}>
+                                                        더보기
+                                                </p>
+                                        </div>
                                 </div>
-                                <div className='category-content'>
-                                        {activeMenu == '회원 관리' && (
-                                                <>
-                                                        <h4 style={{textAlign:'left'}}>탈퇴회원 검색</h4>
-                                                        <div class="row" style={{backgroundColor:'#eeeeee'}}>
-                                                                <div className="col p-2">번호</div>
-                                                                <div className="col p-2">아이디</div>
-                                                                <div className="col p-2">성명</div>
-                                                                <div className="col p-2">이메일</div>
-                                                                <div className="col p-2">연락처</div>
-                                                                <div className="col p-2">가입일</div>
-                                                                <div className="col p-2">관리</div>
+                        )}
+
+                        {/* 기업관리 페이지 */}
+                        {activeMenu == '기업 관리' &&(
+                                <div style={{display: 'flex', flexDirection: 'column', width:'900px'}}>
+                                        <div className='category-content'>
+                                                <h4 style={{textAlign:'left', fontWeight:'600'}}>기업 검색</h4>
+                                                <div class="row" style={{backgroundColor:'#eeeeee', fontSize:'0.8em'}}>
+                                                        <div className="col p-2">번호</div>
+                                                        <div className="col p-2">아이디</div>
+                                                        <div className="col p-2">성명</div>
+                                                        <div className="col p-2">이메일</div>
+                                                        <div className="col p-2">연락처</div>
+                                                        <div className="col p-2">가입일</div>
+                                                        <div className="col p-2">관리</div>
+                                                </div>
+                                                <div class="row" style={{fontSize:'0.8em'}}>
+                                                        <div className="col p-2">*번호</div>
+                                                        <div className="col p-2">*아이디</div>
+                                                        <div className="col p-2">*성명</div>
+                                                        <div className="col p-2">*이메일</div>
+                                                        <div className="col p-2">*연락처</div>
+                                                        <div className="col p-2">*가입일</div>
+                                                        <div className="col p-2">*관리</div>
+                                                </div>
+                                                <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                                                        <div style={{textDecoration:'underline', textAlign:'left', fontSize:'0.8em'}}>
+                                                                더보기
                                                         </div>
-                                                        <div class="row">
-                                                                <div className="col p-2">*번호</div>
-                                                                <div className="col p-2">*아이디</div>
-                                                                <div className="col p-2">*성명</div>
-                                                                <div className="col p-2">*이메일</div>
-                                                                <div className="col p-2">*연락처</div>
-                                                                <div className="col p-2">*가입일</div>
-                                                                <div className="col p-2">*관리</div>
+                                                        <div className='button'>탈퇴처리</div>
+                                                </div>
+                                        </div>
+                                        <div className='category-content'>
+                                                <h4 style={{textAlign:'left', fontWeight:'600'}}>탈퇴기업 검색</h4>
+                                                <div class="row" style={{backgroundColor:'#eeeeee', fontSize:'0.8em'}}>
+                                                        <div className="col p-2">번호</div>
+                                                        <div className="col p-2">아이디</div>
+                                                        <div className="col p-2">성명</div>
+                                                        <div className="col p-2">이메일</div>
+                                                        <div className="col p-2">연락처</div>
+                                                        <div className="col p-2">가입일</div>
+                                                        <div className="col p-2">관리</div>
+                                                </div>
+                                                <div class="row" style={{fontSize:'0.8em'}}>
+                                                        <div className="col p-2">*번호</div>
+                                                        <div className="col p-2">*아이디</div>
+                                                        <div className="col p-2">*성명</div>
+                                                        <div className="col p-2">*이메일</div>
+                                                        <div className="col p-2">*연락처</div>
+                                                        <div className="col p-2">*가입일</div>
+                                                        <div className="col p-2">*관리</div>
+                                                </div>
+                                                <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                                                        <div style={{textDecoration:'underline', textAlign:'left', fontSize:'0.8em'}}>
+                                                                더보기
                                                         </div>
-                                                </>
-                                                )
-                                        }
+                                                </div>
+                                        </div>
                                 </div>
-                        </div>
+                        )}
+                        {/* 상품관리 페이지 */}
+                        {activeMenu == '상품 관리' && (
+                                <div>
+                                        <div className='category-content'>
+                                                <h4 style={{textAlign:'left', fontWeight:'600'}}>상품 검색</h4>
+                                                <hr/>
+                                                <div style={{textAlign:'left'}}>
+                                                        <div style={{display: 'flex', justifyContent: 'space-between', gap:'10px'}}>
+                                                               <p>카테고리 :</p> 
+                                                               <select className="cat1" style={{width:'200px', padding:'3px', borderRadius:'10px', fontSize:'0.8em', height:'40px'}}>
+                                                                        <option value="null">대분류</option>
+                                                                        <option value="category">카테고리1</option>
+                                                                        <option value="category">카테고리2</option>
+                                                               </select>
+                                                               <select className="cat1" style={{width:'200px', padding:'3px', borderRadius:'10px', fontSize:'0.8em', height:'40px'}}>
+                                                                        <option value="null">소분류</option>
+                                                                        <option value="category">카테고리1</option>
+                                                                        <option value="category">카테고리2</option>
+                                                               </select>
+                                                        </div>
+                                                        <div style={{display: 'flex', justifyContent: 'space-between', gap:'10px'}}>
+                                                               <p>검색어 :</p>
+                                                               <select className="cat1" style={{width:'200px', padding:'3px', borderRadius:'10px', fontSize:'0.8em', height:'40px'}}>
+                                                                        <option value="null">전체</option>
+                                                                        <option value="category">상품명</option>
+                                                                        <option value="category">기업명</option>
+                                                               </select>
+                                                               <input type='text' className='search-text'></input>
+                                                        </div>
+                                                        
+                                                        <p>검색어 :</p>
+                                                        <p>등록일자 :</p>
+                                                </div>
+                                                
+                                        </div>
+                                </div>
+                        )}
+                        {/* 게시글관리 페이지 */}
+                        {activeMenu == '게시글 관리' && (
+                                <div>
+                                        <div className='category-content'>
+                                                <h4 style={{textAlign:'left', fontWeight:'600'}}>게시글 검색</h4>
+                                        </div>
+                                </div>
+                        )}
+                        {/* 문의관리 페이지 */}
+                        {activeMenu == '문의 관리' && (
+                                <div>
+                                        <div className='category-content'>
+                                                <h4 style={{textAlign:'left', fontWeight:'600'}}>문의 검색</h4>
+                                        </div>
+                                </div>
+                        )}
+                        {/* 통계 페이지 */}
+                        {activeMenu == '통계' && (
+                                <div>
+                                        <div className='category-content'>
+                                                <h4 style={{textAlign:'left', fontWeight:'600'}}>통계 조건 검색</h4>
+                                        </div>
+                                </div>
+                        )}
+                        {/* 환경설정 페이지 */}
+                        {activeMenu == '환경설정' && (
+                                <div>
+                                </div>
+                        )}
                 </div>
         )
 }
